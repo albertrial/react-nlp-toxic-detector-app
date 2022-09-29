7a189d7f424ac2583118ee4e4413457f75bffce7
Albert Rial <albert.rial@est.fib.upc.edu>
2022-09-29 21:40:01 +0200

    Starting home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,15 @@
 
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages';
-import About from './pages/about';
-import Events from './pages/events';
-import AnnualReport from './pages/annual';
-import Teams from './pages/team';
-import Blogs from './pages/blogs';
-import SignUp from './pages/signup';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Home from './pages/home'
   
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' exact component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/events' component={Events} />
-        <Route path='/annual' component={AnnualReport} />
-        <Route path='/team' component={Teams} />
-        <Route path='/blogs' component={Blogs} />
-        <Route path='/sign-up' component={SignUp} />
-      </Routes>
-    </Router>
+    <Home></Home>
   );
 }
   
---
+++ app/src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+  
+const Home = () => {
+  return (
+    <div>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </div>
+  );
+};
+  
+export default Home;
