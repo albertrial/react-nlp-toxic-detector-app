0b41255e6f5b6db239d08e50e3ee66ca0b86b788
Albert Rial <albert.rial@est.fib.upc.edu>
2022-12-11 16:19:43 +0100

    Adding loading spinner and introduction to the main page

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -12,13 +12,17 @@ import ThumbUpIcon from '@mui/icons-material/ThumbUpRounded';
 import Box from "@mui/material/Box";
 import logo from "../assets/images/logo.png";
 import { FormControl, InputLabel, MenuItem, Select, List, ListItem, ListItemText, Divider, Grid } from '@mui/material';
+import { PulseLoader } from 'react-spinners';
+import IntroductionGrid from '../components/IntroductionGrid';
 
 const WIDTH = 800;
-  
+
 const Home = () => {
   const [lang, setLang] = React.useState('');
   const [items, setItems] = React.useState([]);
   const [text, setText] = React.useState('');
+  const [isIntroShown, setIsIntroShown] = React.useState(true);
+  const [isLoading, setLoading] = React.useState(false);
 
   const handleLanguageChange = (event) => {
     setLang(event.target.value);
@@ -30,34 +34,37 @@ const Home = () => {
 
   async function getModel(lang) {
     try {
-      const response = await axios.get("toxic_detection/models/"+lang);
+      const response = await axios.get("toxic_detection/models/" + lang);
       console.log("Response");
       console.log(response);
       return response;
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
   }
 
   async function detectToxicSpans(origText) {
     try {
       setItems([...items, origText])
-      const item = {text: origText}
-      const { response } = axios.post(API_URL+"detect", item)
-                          .then((response) => {
-                            console.log(response);
-                            console.log(response.data);
-                            setItems([...items, origText, response.data]);
-                          })
-                          .catch(err => {
-                            console.log(err);
-                          });      
+      const item = { text: origText }
+      const { response } = axios.post(API_URL + "detect", item)
+        .then((response) => {
+          console.log(response);
+          console.log(response.data);
+          setLoading(false);
+          setItems([...items, origText, response.data]);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
   }
 
   function submit() {
+    setIsIntroShown(false);
+    setLoading(true);
     console.log(lang + " " + text);
     //const model = getModel(lang);
     detectToxicSpans(text);
@@ -84,56 +91,70 @@ const Home = () => {
 
   return (
     <Box align="center">
-      <img 
-          alt="Bleeptox logo" 
-          src={logo}
-          style={{width: 400,
-                  marginTop: 50,
-                  marginBottom: 50}} />
-        <List
-          flex-direction="column"
-          display="flex"
-          height="100%"
-          alignItems="center">
-          {items.map((item, index) => (
-            <Box 
-              width="100%" 
-              display="block" 
-              key={index} 
-              style={{ backgroundColor: index%2 === 0 ? 'white' : '#F7F7F8' }}>
-              <ListItem display="flex" key={index} style={{maxWidth: WIDTH}}>
+      <img
+        alt="Bleeptox logo"
+        src={logo}
+        style={{
+          width: 350,
+          marginTop: 20,
+          marginBottom: 20
+        }} />
+      {isIntroShown ? ( <IntroductionGrid id="introduction-grid"/>) : (<div/>)}
+      <List
+        id="text-list"
+        flex-direction="column"
+        display="flex"
+        height="100%"
+        alignItems="center">
+        {items.map((item, index) => (
+          <Box
+            width="100%"
+            display="block"
+            key={index}
+            style={{ 
+              backgroundColor: index % 2 === 0 ? 'white' : '#F7F7F8',
+              marginRight: 3,
+              marginLeft: 3
+            }}>
+            <ListItem display="flex" key={index} style={{ maxWidth: WIDTH }}>
               <Grid container>
                 <Grid item xs={10}>
                   <ListItemText
-                      primary={item} style= {{wordWrap: "break-word" }}/>
+                    primary={item} style={{ wordWrap: "break-word" }} />
                 </Grid>
                 <Grid item xs={2}
                   justify="flex-end"
                   alignItems="center">
-                  <IconButton 
-                        id="approve"
-                        variant="contained"
-                        onClick={handleApprove}>
-                        <ThumbUpIcon fontSize='small'/>
+                  <IconButton
+                    id="approve"
+                    variant="contained"
+                    onClick={handleApprove}>
+                    <ThumbUpIcon fontSize='small' />
                   </IconButton>
                   <IconButton
-                      id="reject"
-                      variant="contained" 
-                      onClick={handleReject}>
-                      <ThumbDownIcon fontSize='small'/>
+                    id="reject"
+                    variant="contained"
+                    onClick={handleReject}>
+                    <ThumbDownIcon fontSize='small' />
                   </IconButton>
                 </Grid>
               </Grid>
-              </ListItem>
-              <Divider />
-            </Box>
-          ))}
-        </List>
+            </ListItem>
+            <Divider />
+          </Box>
+        ))}
+        <br></br>
+        <PulseLoader
+          size={10}
+          color={'#176FB8'}
+          loading={isLoading} 
+        />
+      </List>
       
       <TextField
+        id="input-text"
         variant="outlined"
         value={text}
-        id="input-text"
         label="Type something here"
         minRows={1}
         maxRows={20}
@@ -148,16 +169,17 @@ const Home = () => {
           right: 0,
           margin: 'auto'
         }}
-        InputProps={{endAdornment:
-                    <IconButton 
-                      id="send-button" 
-                      onClick={handleSubmit}>
-                      <SendIcon fontSize='small'/>
-                    </IconButton> 
-                    }}
-        />
+        InputProps={{
+          endAdornment:
+            <IconButton
+              id="send-button"
+              onClick={handleSubmit}>
+              <SendIcon fontSize='small' />
+            </IconButton>
+        }}
+      />
     </Box>
   );
 };
-  
+
 export default Home;
